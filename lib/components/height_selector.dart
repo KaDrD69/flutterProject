76c650c6f8e1c7756ec77bd0b659973597c0b8b3
KaDrD <kadrd@localhost.localdomain>
2025-06-06 18:27:02 -0300

    Agregados widgets peso edad, falta crear la lógica

--- lib/components/height_selector.dart
+++ lib/components/height_selector.dart
@@ -14,25 +14,41 @@ class _HeightSelectorState extends State<HeightSelector> {
   
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text("Altura", style: TextStyles.bodyStyle),
-        Text("${height.toStringAsFixed(0)} cm", style: TextStyles.bodyStyle),
-        Slider(
-            value: height,
-            onChanged: (newHeight){
-              setState(() {
-                height = newHeight;
-              });
-            }, 
-            min: 150,
-            max: 220,
-            divisions: 70,
-            label: "${height.toStringAsFixed(0)} cm",
-            activeColor: AppColors.primary,
-
-        )
-      ],
+    return Padding(
+      padding: const EdgeInsets.only(left: 16, right: 16),
+      child: Container(
+        decoration: BoxDecoration(
+          color: AppColors.backgroundComponent,
+          borderRadius: BorderRadius.circular(16)
+        ),
+        child: Column(
+          children: [
+            Text("Altura", style: TextStyles.bodyStyle),
+            Text(
+              "${height.toStringAsFixed(0)} cm",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 37,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Slider(
+                value: height,
+                onChanged: (newHeight){
+                  setState(() {
+                    height = newHeight;
+                  });
+                }, 
+                min: 150,
+                max: 220,
+                divisions: 70,
+                label: "${height.toStringAsFixed(0)} cm",
+                activeColor: AppColors.primary,
+        
+            )
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/screens/imc_home_screen.dart
+++ lib/screens/imc_home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:imc_calculadora/components/gender_selector.dart';
 import 'package:imc_calculadora/components/height_selector.dart';
+import 'package:imc_calculadora/components/number_selector.dart';
 
 class ImcHomeScreen extends StatefulWidget {
   const ImcHomeScreen({super.key});
@@ -16,6 +17,16 @@ class _ImcHomeScreenState extends State<ImcHomeScreen> {
       children: [
         GenderSelector(),
         HeightSelector(),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Row(
+            children: [
+              Expanded(child: NumberSelector(title: "PESO")),
+              SizedBox(width: 16),
+              Expanded(child: NumberSelector(title: "EDAD")),
+            ],
+          ),
+        ),
       ],
     );
   }
